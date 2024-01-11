bf18fd1f0fca7e024037c7a57b09bd4a4996f677
Om Dhingani <omdhingani0@gmail.com>
2024-01-11 20:19:19 +0530

    User Rest Apis

--- src/main/java/com/local/sdp/REST/UserRESTController.java
+++ src/main/java/com/local/sdp/REST/UserRESTController.java
@@ -3,7 +3,6 @@ package com.local.sdp.REST;
 
 import com.local.sdp.Entity.User;
 import com.local.sdp.ExceptionHandlers.ExceptionResponse;
-import com.local.sdp.ExceptionHandlers.SpecificExceptions.UserNotFound;
 import com.local.sdp.Services.Interface.UserServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,24 +28,12 @@ public class UserRESTController {
 
     @GetMapping("/users/id/{userId}")
     public User findById(@PathVariable int userId) {
-        User user = userService.findById(userId);
-
-        if(user == null) {
-            throw  new UserNotFound("User not found " + userId);
-        }
-
-        return user;
+        return userService.findById(userId);
     }
 
     @GetMapping("/users/email/{userEmail}")
     public User findByEmail(@PathVariable String userEmail) {
-        User user = userService.findByEmail(userEmail);
-
-        if(user == null) {
-            throw new UserNotFound("User not found : " + userEmail);
-        }
-
-        return user;
+        return userService.findByEmail(userEmail);
     }
 
     @GetMapping("/users/type/{userType}")
@@ -69,11 +56,6 @@ public class UserRESTController {
     @DeleteMapping("/users/{userId}")
     public String delete(@PathVariable int userId) {
         User user = userService.findById(userId);
-
-        if(user == null) {
-            throw  new UserNotFound("User not found : " + userId);
-        }
-
         userService.delete(userId);
         return "Deleted";
     }
@@ -82,21 +64,12 @@ public class UserRESTController {
     // Exception Handling
 
     @ExceptionHandler
-    public ResponseEntity<ExceptionResponse> userNotFound(UserNotFound exception) {
-        ExceptionResponse error = new ExceptionResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exception.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ExceptionResponse> badRequest(java.lang.Exception exception) {
-        ExceptionResponse error = new ExceptionResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exception.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ExceptionResponse> handleException(Exception ex){
+        ExceptionResponse exceptionResponse = new ExceptionResponse();
+        exceptionResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        exceptionResponse.setMessage("User Exception : " + ex.getMessage());
+        exceptionResponse.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
